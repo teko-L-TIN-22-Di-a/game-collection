15a5fe7a4f3bbafbce451adbff37b83e19916517
Manu <e.manu@windowslive.com>
2023-07-25 19:05:16 +0200

    Progress

---
+++ TicTacToe/src/ch/teko/loefflee/GameBoard.java
@@ -0,0 +1,14 @@
+
+package ch.teko.loefflee;
+
+
+public class GameBoard {
+    
+    char [][] gameBoard ={    {' ', ' ', ' '},
+                              {' ', ' ', ' '}, 
+                              {' ', ' ', ' '},};
+
+    void setvalue(int line, int column){
+        gameBoard[line][column] = 'X';
+    }  
+}
